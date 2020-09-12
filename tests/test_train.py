3180b86a23af05df84b29a547f1dbf51d463ba06
jeremy cunningham <j2cunningham@yahoo.com>
2020-09-12 11:06:52 -0500

    added a few more test

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,15 +1,31 @@
 import sys
 sys.path.append('/home/jeremy/gitlab/CodeSnips/train_model')
-from train import setX
+from train import setX, sety, readFile
 import pandas as pd
 import pytest
 
-def test_setx():
+@pytest.fixture
+def getDF():
     data = [['Alex',10],['Bob',12],['Clarke',13]]
     df = pd.DataFrame(data,columns=['Name','IsAdoptedUser'])
-    df = setX(df)
-    print(df.shape)
+    return df
+
+def test_fixture(getDF):
+    assert type(getDF) != None
+
+def test_setx(getDF):
+    df = setX(getDF)
     assert df.shape == (3,1)
+
+def test_sety(getDF):
+    df = sety(getDF)
+    assert df.name == 'IsAdoptedUser'
+
+def test_readFile():
+    tuple = readFile()
+    assert len(tuple) == 2
+
+
 
 # https://towardsdatascience.com/unit-testing-for-data-scientists-dc5e0cd397fb
 
